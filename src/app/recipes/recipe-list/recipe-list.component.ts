838e482305524c4ddd3d02a458024dcee904e2d7
Rishikesh Krishnan <rishikeshnlr@gmail.com>
2021-10-28 00:27:49 +0400

    second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test', 'Sample Desc', "https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg")
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
